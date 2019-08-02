5cdc481263583ffab739b4a31611c023ff580176
Pavel Poltaves <dzhura17@yandex.ru>
2019-08-02 20:40:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main()
 {
-unsigned int a ;
-cout<<"Count of stars: ";
-cin>>a;
-for(;a>0;a--) cout<< "*";
-return 0;
+    unsigned int a ;
+    cout<<"Count of stars: ";
+    cin>>a;
+    for(;a>0;a--) cout<< "*";
+    return 0;
 }
 
